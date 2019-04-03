6b7b5804fe155c22e3eb97f1d50c5d287a3f1df2
Cazajous Miguel A <migue7490@gmail.com>
2019-04-03 17:49:48 -0300

    Minor changes

--- .vim/templates/template.cpp
+++ .vim/templates/template.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+class temp
+{
+	public:
+		temp (int valur);
+		virtual ~temp ();
+
+	private:
+		/* data */
+};
+
 int main(int argc, char **argv){
-    return 0;
+	return 0;
+
 }
--- .vimrc
+++ .vimrc
@@ -46,6 +46,7 @@ set number
 set showcmd "Show commands
 set incsearch "Highlight text in searches
 set tabstop=4 "tab 4 spaces
+set shiftwidth=4 "indent
 
 "Color scheme
 set termguicolors
