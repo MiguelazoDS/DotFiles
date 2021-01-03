fde4d957979b3974e59db0fbae0629f55f99ac6a
Cazajous Miguel A <migue7490@gmail.com>
2021-01-02 23:29:49 -0300

    Show hidden files fzf ranger

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -127,8 +127,7 @@ class fzf_select(Command):
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         else:
             # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            command = "find . -printf '%P\\n'| fzf -m"
         fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
