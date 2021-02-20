f758f9048a6c4a8ae467e0902a4db330fd78436d
Cazajous Miguel A <migue7490@gmail.com>
2021-02-20 01:12:07 -0300

    Added two more workspaces

--- .xmonad/lib/Workspaces.hs
+++ .xmonad/lib/Workspaces.hs
@@ -4,12 +4,15 @@ module Workspaces
 
 import Data.List as L
 
+n :: Int
+n = 7
+
 xmobarEscape = concatMap doubleLts
   where
         doubleLts x   = [x]
 
 myWorkspaces :: [String]
-myWorkspaces = clickable $ replicate 5 $ xmobarEscape "<icon=workspace.xpm/>"
+myWorkspaces = clickable $ replicate n $ xmobarEscape "<icon=workspace.xpm/>"
    where
          clickable l = [ "<action=xdotool key super+" ++ show n ++ ">" ++ ws ++ "</action>" |
-                       (i,ws) <- zip [1..5] l, let n = i ]
+                       (i,ws) <- zip [1..n] l, let n = i ]
