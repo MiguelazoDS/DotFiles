9e47ba60fbaea032aa20c76769f81af4258a6c06
Cazajous Miguel A <migue7490@gmail.com>
2021-11-14 19:07:17 -0300

    Added style

---
+++ .config/nvim/lua/style.lua
@@ -0,0 +1,81 @@
+-- ============================================================
+-- nvim devicons configuration
+-- ===========================================================
+
+require'nvim-web-devicons'.setup {
+ -- your personnal icons can go here (to override)
+ -- DevIcon will be appended to `name`
+ override = {
+  zsh = {
+    icon = "",
+    color = "#428850",
+    name = "Zsh"
+  }
+ };
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+}
+
+-- ===========================================================
+-- Lspkind configuration
+-- ===========================================================
+
+require('lspkind').init({
+    -- enables text annotations
+    --
+    -- default: true
+    with_text = true,
+
+    -- default symbol map
+    -- can be either 'default' (requires nerd-fonts font) or
+    -- 'codicons' for codicon preset (requires vscode-codicons font)
+    --
+    -- default: 'default'
+    preset = 'codicons',
+
+    -- override preset symbols
+    --
+    -- default: {}
+    symbol_map = {
+      Text = "",
+      Method = "",
+      Function = "",
+      Constructor = "",
+      Field = "ﰠ",
+      Variable = "",
+      Class = "ﴯ",
+      Interface = "",
+      Module = "",
+      Property = "ﰠ",
+      Unit = "塞",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "פּ",
+      Event = "",
+      Operator = "",
+      TypeParameter = ""
+    },
+})
+
+-- ============================================================
+-- Bufferline configuration.
+-- ============================================================
+
+require("bufferline").setup{
+    diagnostics = "nvim_lsp",
+	diagnostics_indicator = function(count, level, diagnostics_dict, context)
+		local icon = level:match("error") and " " or " "
+	    return " " .. icon .. count
+	end
+}
+
+require'colorizer'.setup()
