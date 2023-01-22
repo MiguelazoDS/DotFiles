654e84e2a4673ced33b593d0a59e2aa6e65e6ad7
Cazajous Miguel A <migue7490@gmail.com>
2023-01-22 18:13:39 -0300

    Using border in active/inactive workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -10,19 +10,30 @@ import XMonad.Config.Desktop
 import System.IO -- (hPutStrLn)
 import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)
 import XMonad.Util.Run (safeSpawn, unsafeSpawn, runInTerm, spawnPipe)
-import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, pad, xmobarPP, xmobarColor, shorten, PP(..))
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, pad, xmobarPP, xmobarColor, xmobarBorder, shorten, PP(..))
+import           XMonad.Hooks.StatusBar.PP           (PP (..), filterOutWsPP,
+                                                      shorten', wrap,
+                                                      xmobarAction,
+                                                      xmobarBorder, xmobarColor,
+                                                      xmobarFont, xmobarStrip)
 import XMonad.Hooks.ManageDocks (avoidStruts, docksStartupHook, manageDocks, ToggleStruts(..))
 import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCenterFloat)
 import XMonad.Hooks.EwmhDesktops   -- required for xcomposite in obs to work
+import Basement.Types.Char7 (c7_1)
+
+wrapSep :: String -> String
+wrapSep = wrap (xmobarColor "#717733" "" (xmobarFont 5 "\xe0b6"))
+               (xmobarColor "#717733" "" (xmobarFont 5 "\xe0b4"))
 
 main = do
-    xmproc <- spawnPipe $ "xmobar $HOME/.config/xmobar/xmobarrc"
+    xmproc <- spawnPipe "xmobar $HOME/.config/xmobar/xmobarrc"
     xmonad $ ewmh desktopConfig
         { manageHook = ( isFullscreen --> doFullFloat ) <+> myManageHook <+> manageHook desktopConfig <+> manageDocks
         , logHook = dynamicLogWithPP xmobarPP
-              { ppOutput = hPutStrLn xmproc
-              , ppCurrent = xmobarColor "#717733" "" . wrap "<{" "}>" -- Current workspace in xmobar
-              , ppHidden = xmobarColor "#A54242" "" . wrap "*" ""   -- Hidden workspaces in xmobar
+              { -- ppSep = wrapSep " "
+              ppOutput = hPutStrLn xmproc
+              , ppCurrent = xmobarColor "#717733" "" . xmobarBorder "Bottom" "#717733" 4 -- . wrap "<{" "}>" -- Current workspace in xmobar
+              , ppHidden = xmobarColor "#A54242" "" . xmobarBorder "Bottom" "#A54242" 4 --wrap "*" ""   -- Hidden workspaces in xmobar
               , ppHiddenNoWindows = xmobarColor "#DEB88D" ""        -- Hidden workspaces (no windows)
               , ppSep =  "<fc=#FBA02F> </fc>"                     -- Separators in xmobar
               , ppUrgent = xmobarColor "#303030" "" . wrap "!" "!"  -- Urgent workspace
