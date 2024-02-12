a1192e07a9589b486cca2f78cb98f15f7ba88c8c
Cazajous Miguel A. <migue7490@gmail.com>
2024-02-12 17:24:29 -0300

    Formatting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,16 +1,16 @@
---require'lspconfig'.asm_lsp.setup{
-    --root_dir = require('lspconfig.util').root_pattern('./'),
-    --filetypes = {"nasm"}
---}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.cmake.setup{}
-require'lspconfig'.diagnosticls.setup{}
-require'lspconfig'.grammarly.setup{}
+require 'lspconfig'.asm_lsp.setup {
+  root_dir = require('lspconfig.util').root_pattern('./'),
+  filetypes = { "nasm" }
+}
+require 'lspconfig'.bashls.setup {}
+require 'lspconfig'.clangd.setup {}
+require 'lspconfig'.cmake.setup {}
+require 'lspconfig'.diagnosticls.setup {}
+require 'lspconfig'.grammarly.setup {}
 --require'lspconfig'.hls.setup{}
-require'lspconfig'.jsonls.setup{}
-require'lspconfig'.lemminx.setup{}
-require'lspconfig'.lua_ls.setup {
+require 'lspconfig'.jsonls.setup {}
+require 'lspconfig'.lemminx.setup {}
+require 'lspconfig'.lua_ls.setup {
   settings = {
     Lua = {
       runtime = {
@@ -19,7 +19,7 @@ require'lspconfig'.lua_ls.setup {
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = { 'vim' },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
@@ -32,92 +32,94 @@ require'lspconfig'.lua_ls.setup {
     },
   },
 }
-require'lspconfig'.marksman.setup{}
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.sqlls.setup{}
-require'lspconfig'.texlab.setup{}
-require'lspconfig'.yamlls.setup{}
-require'lspconfig'.veridian.setup{
-    root_dir = require('lspconfig.util').root_pattern('./'),
-    filetypes = {"verilog"}
+require 'lspconfig'.marksman.setup {}
+require 'lspconfig'.pyright.setup {}
+require 'lspconfig'.sqlls.setup {
+  root_dir = require('lspconfig.util').root_pattern('./'),
 }
-require'lspconfig'.autotools_ls.setup{}
+require 'lspconfig'.texlab.setup {}
+require 'lspconfig'.yamlls.setup {}
+require 'lspconfig'.veridian.setup {
+  root_dir = require('lspconfig.util').root_pattern('./'),
+  filetypes = { "verilog" }
+}
+require 'lspconfig'.autotools_ls.setup {}
 
-vim.g['LanguageClient_serverCommands'] = { 'sql',  {'sql-language-server', 'up', '--method', 'stdio'} }
+vim.g['LanguageClient_serverCommands'] = { 'sql', { 'sql-language-server', 'up', '--method', 'stdio' } }
 
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = { "autotools_ls",
-                         --"asm_lsp",
-                         "bashls",
-                         "clangd",
-                         "cmake",
-                         "diagnosticls",
-                         "grammarly",
-                         --"hls",
-                         "jsonls",
-                         "lemminx",
-                         "lua_ls",
-                         "marksman",
-                         "pyright",
-                         --"sqlls",
-                         "texlab"}, -- ensure these servers are always installed
-    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  ensure_installed = { "autotools_ls",
+    "asm_lsp",
+    "bashls",
+    "clangd",
+    "cmake",
+    "diagnosticls",
+    "grammarly",
+    --"hls",
+    "jsonls",
+    "lemminx",
+    "lua_ls",
+    "marksman",
+    "pyright",
+    "sqlls",
+    "texlab" },                     -- ensure these servers are always installed
+  automatic_installation = true,    -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
+  }
 }
 
 require('lspkind').init({
-    -- DEPRECATED (use mode instead): enables text annotations
-    --
-    -- default: true
-    -- with_text = true,
+  -- DEPRECATED (use mode instead): enables text annotations
+  --
+  -- default: true
+  -- with_text = true,
 
-    -- defines how annotations are shown
-    -- default: symbol
-    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-    mode = 'symbol_text',
+  -- defines how annotations are shown
+  -- default: symbol
+  -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+  mode = 'symbol_text',
 
-    -- default symbol map
-    -- can be either 'default' (requires nerd-fonts font) or
-    -- 'codicons' for codicon preset (requires vscode-codicons font)
-    --
-    -- default: 'default'
-    preset = 'codicons',
+  -- default symbol map
+  -- can be either 'default' (requires nerd-fonts font) or
+  -- 'codicons' for codicon preset (requires vscode-codicons font)
+  --
+  -- default: 'default'
+  preset = 'codicons',
 
-    -- override preset symbols
-    --
-    -- default: {}
-    symbol_map = {
-      Text = "",
-      Method = "",
-      Function = "",
-      Constructor = "",
-      Field = "ﰠ",
-      Variable = "",
-      Class = "ﴯ",
-      Interface = "",
-      Module = "",
-      Property = "ﰠ",
-      Unit = "塞",
-      Value = "",
-      Enum = "",
-      Keyword = "",
-      Snippet = "",
-      Color = "",
-      File = "",
-      Reference = "",
-      Folder = "",
-      EnumMember = "",
-      Constant = "",
-      Struct = "פּ",
-      Event = "",
-      Operator = "",
-      TypeParameter = ""
-    },
+  -- override preset symbols
+  --
+  -- default: {}
+  symbol_map = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "ﰠ",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "塞",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "פּ",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+  },
 })
