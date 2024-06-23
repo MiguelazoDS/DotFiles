20b37e71e0a53908f34c909c70eb8435961d0d51
Cazajous Miguel A. <migue7490@gmail.com>
2024-06-23 13:09:34 -0300

    Improve workspace tab information

--- .config/xmonad/lib/Workspaces.hs
+++ .config/xmonad/lib/Workspaces.hs
@@ -4,8 +4,11 @@ module Workspaces
 
 import Data.List as L
 
-n :: Int
-n = 7
+workspaceNumber :: Int
+workspaceNumber = 7
+
+icon :: String
+icon = "\62060 "
 
 xmobarEscape = concatMap doubleLts
   where
@@ -15,7 +18,7 @@ workspaceColor :: String
 workspaceColor = "#5ac1db"
 
 myWorkspaces :: [String]
-myWorkspaces = clickable $ replicate n $ xmobarEscape "<fc=" ++ workspaceColor ++ "><fn=2>\987396</fn></fc>"
+myWorkspaces = clickable $ replicate workspaceNumber $ xmobarEscape "<fc=" ++ workspaceColor ++ "><fn=2>" ++ icon ++ "</fn></fc>"
    where
-         clickable l = [ "<action=xdotool key super+" ++ show n ++ ">" ++ ws ++ "</action>" |
-                       (i,ws) <- zip [1..n] l, let n = i ]
+         clickable l = [ "<action=xdotool key super+" ++ show workspaceNumber ++ ">" ++ ws ++ "</action>" |
+                       (i,ws) <- zip [1..workspaceNumber] l, let workspaceNumber = i ]
