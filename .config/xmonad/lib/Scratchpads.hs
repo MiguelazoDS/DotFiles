ddd6cd97bd4e4cae8c4a895239c8dab7d3be7d80
Cazajous Miguel A. <migue7490@gmail.com>
2024-10-27 21:16:09 -0300

    Increase scratchpad window size

--- .config/xmonad/lib/Scratchpads.hs
+++ .config/xmonad/lib/Scratchpads.hs
@@ -15,14 +15,15 @@ myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
     where
     spawnTerm  = M.myTerminal M.applications ++ " start --class scratchpad"
     findTerm   = resource =? "scratchpad"
-    manageTerm = customFloating $ W.RationalRect l t w h
+    manageTerm = customFloating $ W.RationalRect x y w h
     spawnMocp  = M.myTerminal M.applications ++  " start --class=moc --always-new-process 'mocp'"
     findMocp   = resource =? "moc"
-    manageMocp = customFloating $ W.RationalRect l t w h
+    manageMocp = customFloating $ W.RationalRect x y w h
     spawnEffects = "easyeffects"
     findEffects = resource =? "easyeffects"
-    manageEffects = customFloating $ W.RationalRect l t w h
-    h = 0.9
-    w = 0.9
-    t = 0.95 -h
-    l = 0.95 -w
+    manageEffects = customFloating $ W.RationalRect x y w h
+    x = 0.01
+    y = 0.01
+    w = 0.98
+    h = 0.98
+
