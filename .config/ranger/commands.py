3b0d2e7d6164dcb270037703f7488eb5c2086953
Cazajous Miguel A. <migue7490@gmail.com>
2024-11-18 16:55:02 -0300

    Fix fzf in ranger

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -96,7 +96,7 @@ class fzf_select(Command):
         import os.path
         if self.quantifier:
             # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = f"find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         else:
             # match files and directories
