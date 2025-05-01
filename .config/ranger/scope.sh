1b28f0824136f3b6960703266add15c2391a0a32
Cazajous Miguel A. <migue7490@gmail.com>
2025-05-01 17:47:56 -0300

    Avoid preview for compressed files greater than 1G

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -44,8 +44,10 @@ handle_extension() {
         # Archive
         a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
         rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
-            atool --list -- "${FILE_PATH}" && exit 5
-            bsdtar --list --file "${FILE_PATH}" && exit 5
+            if [[ ! "$(du -sh ${FILE_PATH})" =~ "G" ]]; then
+              atool --list -- "${FILE_PATH}" && exit 5
+              bsdtar --list --file "${FILE_PATH}" && exit 5
+            fi
             exit 1;;
         rar)
             # Avoid password prompt by providing empty password
